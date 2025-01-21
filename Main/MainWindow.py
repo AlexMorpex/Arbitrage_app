3bd30f03e7fc25a54248fc4bb64ab0f0ecd7b068
Morpex_LT <alexandrmorpex@gmail.com>
2025-01-21 16:29:55 +0200

    Ui

--- Main/MainWindow.py
+++ Main/MainWindow.py
@@ -15,19 +15,20 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QComboBox, QHBoxLayout, QLabel,
-    QLayout, QMainWindow, QPushButton, QSizePolicy,
-    QSpacerItem, QVBoxLayout, QWidget)
+from PySide6.QtWidgets import (QApplication, QHBoxLayout, QLayout, QMainWindow,
+    QPushButton, QSizePolicy, QSpacerItem, QStackedWidget,
+    QVBoxLayout, QWidget)
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         if not MainWindow.objectName():
             MainWindow.setObjectName(u"MainWindow")
-        MainWindow.resize(915, 624)
+        MainWindow.resize(1018, 624)
         MainWindow.setMinimumSize(QSize(800, 500))
         MainWindow.setStyleSheet(u"")
         self.MainWidget = QWidget(MainWindow)
         self.MainWidget.setObjectName(u"MainWidget")
+        self.MainWidget.setMinimumSize(QSize(1040, 624))
         self.MainWidget.setStyleSheet(u"border:1px solid;\n"
 "border-color: rgb(0, 255, 0);")
         self.verticalLayout = QVBoxLayout(self.MainWidget)
@@ -43,46 +44,57 @@ class Ui_MainWindow(object):
         self.Head.setSizePolicy(sizePolicy)
         self.Head.setMinimumSize(QSize(0, 70))
         self.Head.setMaximumSize(QSize(16777215, 70))
-        self.Head.setStyleSheet(u"layout-spacing:10px;")
+        self.Head.setStyleSheet(u"layout-spacing:10px;\n"
+"background-color: rgb(0, 26, 24)")
         self.horizontalLayout_2 = QHBoxLayout(self.Head)
         self.horizontalLayout_2.setObjectName(u"horizontalLayout_2")
         self.horizontalLayout_2.setSizeConstraint(QLayout.SizeConstraint.SetDefaultConstraint)
-        self.pushButton = QPushButton(self.Head)
-        self.pushButton.setObjectName(u"pushButton")
+        self.Menu_button = QPushButton(self.Head)
+        self.Menu_button.setObjectName(u"Menu_button")
         sizePolicy1 = QSizePolicy(QSizePolicy.Policy.Fixed, QSizePolicy.Policy.Fixed)
         sizePolicy1.setHorizontalStretch(0)
         sizePolicy1.setVerticalStretch(0)
-        sizePolicy1.setHeightForWidth(self.pushButton.sizePolicy().hasHeightForWidth())
-        self.pushButton.setSizePolicy(sizePolicy1)
-        self.pushButton.setMinimumSize(QSize(50, 50))
-        self.pushButton.setMaximumSize(QSize(50, 50))
+        sizePolicy1.setHeightForWidth(self.Menu_button.sizePolicy().hasHeightForWidth())
+        self.Menu_button.setSizePolicy(sizePolicy1)
+        self.Menu_button.setMinimumSize(QSize(50, 50))
+        self.Menu_button.setMaximumSize(QSize(50, 50))
 
-        self.horizontalLayout_2.addWidget(self.pushButton)
+        self.horizontalLayout_2.addWidget(self.Menu_button)
 
-        self.label = QLabel(self.Head)
-        self.label.setObjectName(u"label")
-        self.label.setMinimumSize(QSize(50, 50))
-        self.label.setMaximumSize(QSize(200, 50))
+        self.Main_page_button = QPushButton(self.Head)
+        self.Main_page_button.setObjectName(u"Main_page_button")
+        self.Main_page_button.setMinimumSize(QSize(150, 50))
+        self.Main_page_button.setMaximumSize(QSize(150, 150))
 
-        self.horizontalLayout_2.addWidget(self.label)
+        self.horizontalLayout_2.addWidget(self.Main_page_button)
 
         self.horizontalSpacer_2 = QSpacerItem(40, 20, QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Minimum)
 
         self.horizontalLayout_2.addItem(self.horizontalSpacer_2)
 
-        self.pushButton_6 = QPushButton(self.Head)
-        self.pushButton_6.setObjectName(u"pushButton_6")
-        self.pushButton_6.setMinimumSize(QSize(50, 50))
-        self.pushButton_6.setMaximumSize(QSize(50, 50))
+        self.Settings_button = QPushButton(self.Head)
+        self.Settings_button.setObjectName(u"Settings_button")
+        self.Settings_button.setMinimumSize(QSize(50, 50))
+        self.Settings_button.setMaximumSize(QSize(50, 50))
+        self.Settings_button.setStyleSheet(u"")
 
-        self.horizontalLayout_2.addWidget(self.pushButton_6)
+        self.horizontalLayout_2.addWidget(self.Settings_button)
 
-        self.pushButton_5 = QPushButton(self.Head)
-        self.pushButton_5.setObjectName(u"pushButton_5")
-        self.pushButton_5.setMinimumSize(QSize(50, 50))
-        self.pushButton_5.setMaximumSize(QSize(50, 50))
+        self.Mode_button = QPushButton(self.Head)
+        self.Mode_button.setObjectName(u"Mode_button")
+        self.Mode_button.setMinimumSize(QSize(50, 50))
+        self.Mode_button.setMaximumSize(QSize(50, 50))
+        self.Mode_button.setStyleSheet(u"")
 
-        self.horizontalLayout_2.addWidget(self.pushButton_5)
+        self.horizontalLayout_2.addWidget(self.Mode_button)
+
+        self.Account_button = QPushButton(self.Head)
+        self.Account_button.setObjectName(u"Account_button")
+        self.Account_button.setMinimumSize(QSize(50, 50))
+        self.Account_button.setMaximumSize(QSize(50, 50))
+        self.Account_button.setStyleSheet(u"")
+
+        self.horizontalLayout_2.addWidget(self.Account_button)
 
 
         self.verticalLayout.addWidget(self.Head)
@@ -96,48 +108,46 @@ class Ui_MainWindow(object):
         self.horizontalLayout.setContentsMargins(0, 0, 0, 0)
         self.LeftSide_menu = QWidget(self.Body)
         self.LeftSide_menu.setObjectName(u"LeftSide_menu")
-        self.LeftSide_menu.setMaximumSize(QSize(250, 16777215))
-        self.verticalLayout_2 = QVBoxLayout(self.LeftSide_menu)
-        self.verticalLayout_2.setObjectName(u"verticalLayout_2")
-        self.pushButton_4 = QPushButton(self.LeftSide_menu)
-        self.pushButton_4.setObjectName(u"pushButton_4")
-        self.pushButton_4.setMinimumSize(QSize(230, 50))
-        self.pushButton_4.setMaximumSize(QSize(230, 50))
-
-        self.verticalLayout_2.addWidget(self.pushButton_4)
-
-        self.comboBox = QComboBox(self.LeftSide_menu)
-        self.comboBox.setObjectName(u"comboBox")
-        sizePolicy2 = QSizePolicy(QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Minimum)
+        sizePolicy2 = QSizePolicy(QSizePolicy.Policy.Fixed, QSizePolicy.Policy.Preferred)
         sizePolicy2.setHorizontalStretch(0)
         sizePolicy2.setVerticalStretch(0)
-        sizePolicy2.setHeightForWidth(self.comboBox.sizePolicy().hasHeightForWidth())
-        self.comboBox.setSizePolicy(sizePolicy2)
-        self.comboBox.setMinimumSize(QSize(230, 50))
-        self.comboBox.setMaximumSize(QSize(230, 50))
+        sizePolicy2.setHeightForWidth(self.LeftSide_menu.sizePolicy().hasHeightForWidth())
+        self.LeftSide_menu.setSizePolicy(sizePolicy2)
+        self.LeftSide_menu.setMaximumSize(QSize(185, 16777215))
+        self.LeftSide_menu.setStyleSheet(u"background-color: rgb(0, 55, 50);")
+        self.verticalLayout_2 = QVBoxLayout(self.LeftSide_menu)
+        self.verticalLayout_2.setSpacing(5)
+        self.verticalLayout_2.setObjectName(u"verticalLayout_2")
+        self.verticalLayout_2.setContentsMargins(10, 10, 10, 10)
+        self.Charts_button = QPushButton(self.LeftSide_menu)
+        self.Charts_button.setObjectName(u"Charts_button")
+        sizePolicy1.setHeightForWidth(self.Charts_button.sizePolicy().hasHeightForWidth())
+        self.Charts_button.setSizePolicy(sizePolicy1)
+        self.Charts_button.setMinimumSize(QSize(165, 50))
+        self.Charts_button.setMaximumSize(QSize(180, 50))
 
-        self.verticalLayout_2.addWidget(self.comboBox)
+        self.verticalLayout_2.addWidget(self.Charts_button)
 
-        self.pushButton_7 = QPushButton(self.LeftSide_menu)
-        self.pushButton_7.setObjectName(u"pushButton_7")
-        self.pushButton_7.setMinimumSize(QSize(230, 50))
-        self.pushButton_7.setMaximumSize(QSize(230, 50))
+        self.Exchanges_button = QPushButton(self.LeftSide_menu)
+        self.Exchanges_button.setObjectName(u"Exchanges_button")
+        self.Exchanges_button.setMinimumSize(QSize(165, 50))
+        self.Exchanges_button.setMaximumSize(QSize(180, 50))
 
-        self.verticalLayout_2.addWidget(self.pushButton_7)
+        self.verticalLayout_2.addWidget(self.Exchanges_button)
 
-        self.pushButton_8 = QPushButton(self.LeftSide_menu)
-        self.pushButton_8.setObjectName(u"pushButton_8")
-        self.pushButton_8.setMinimumSize(QSize(230, 50))
-        self.pushButton_8.setMaximumSize(QSize(230, 50))
+        self.Ai_predictions_button = QPushButton(self.LeftSide_menu)
+        self.Ai_predictions_button.setObjectName(u"Ai_predictions_button")
+        self.Ai_predictions_button.setMinimumSize(QSize(165, 50))
+        self.Ai_predictions_button.setMaximumSize(QSize(180, 50))
 
-        self.verticalLayout_2.addWidget(self.pushButton_8)
+        self.verticalLayout_2.addWidget(self.Ai_predictions_button)
 
-        self.pushButton_9 = QPushButton(self.LeftSide_menu)
-        self.pushButton_9.setObjectName(u"pushButton_9")
-        self.pushButton_9.setMinimumSize(QSize(230, 50))
-        self.pushButton_9.setMaximumSize(QSize(230, 50))
+        self.Arbitrage_button = QPushButton(self.LeftSide_menu)
+        self.Arbitrage_button.setObjectName(u"Arbitrage_button")
+        self.Arbitrage_button.setMinimumSize(QSize(165, 50))
+        self.Arbitrage_button.setMaximumSize(QSize(180, 50))
 
-        self.verticalLayout_2.addWidget(self.pushButton_9)
+        self.verticalLayout_2.addWidget(self.Arbitrage_button)
 
         self.verticalSpacer = QSpacerItem(20, 40, QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Expanding)
 
@@ -148,6 +158,17 @@ class Ui_MainWindow(object):
 
         self.Main_menu = QWidget(self.Body)
         self.Main_menu.setObjectName(u"Main_menu")
+        self.Main_menu.setStyleSheet(u"background-color:rgb(49, 80, 77);\n"
+"")
+        self.stackedWidget = QStackedWidget(self.Main_menu)
+        self.stackedWidget.setObjectName(u"stackedWidget")
+        self.stackedWidget.setGeometry(QRect(120, 50, 165, 125))
+        self.Charts_page = QWidget()
+        self.Charts_page.setObjectName(u"Charts_page")
+        self.stackedWidget.addWidget(self.Charts_page)
+        self.Exchenges_page = QWidget()
+        self.Exchenges_page.setObjectName(u"Exchenges_page")
+        self.stackedWidget.addWidget(self.Exchenges_page)
 
         self.horizontalLayout.addWidget(self.Main_menu)
 
@@ -156,8 +177,9 @@ class Ui_MainWindow(object):
 
         self.Footer = QWidget(self.MainWidget)
         self.Footer.setObjectName(u"Footer")
-        self.Footer.setMinimumSize(QSize(0, 70))
-        self.Footer.setMaximumSize(QSize(16777215, 70))
+        self.Footer.setMinimumSize(QSize(0, 60))
+        self.Footer.setMaximumSize(QSize(16777215, 60))
+        self.Footer.setStyleSheet(u"background-color:rgb(0, 26, 24)")
 
         self.verticalLayout.addWidget(self.Footer)
 
@@ -170,13 +192,15 @@ class Ui_MainWindow(object):
 
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"Arbitrage app", None))
-        self.pushButton.setText(QCoreApplication.translate("MainWindow", u"=", None))
-        self.label.setText(QCoreApplication.translate("MainWindow", u"Arbitrage App", None))
-        self.pushButton_6.setText(QCoreApplication.translate("MainWindow", u"Mode", None))
-        self.pushButton_5.setText(QCoreApplication.translate("MainWindow", u"Account", None))
-        self.pushButton_4.setText(QCoreApplication.translate("MainWindow", u"PushButton", None))
-        self.pushButton_7.setText(QCoreApplication.translate("MainWindow", u"PushButton", None))
-        self.pushButton_8.setText(QCoreApplication.translate("MainWindow", u"PushButton", None))
-        self.pushButton_9.setText(QCoreApplication.translate("MainWindow", u"PushButton", None))
+        self.Menu_button.setText(QCoreApplication.translate("MainWindow", u"=", None))
+        self.Main_page_button.setText(QCoreApplication.translate("MainWindow", u"Arbitrage app", None))
+        self.Settings_button.setText(QCoreApplication.translate("MainWindow", u"Settings", None))
+        self.Mode_button.setText(QCoreApplication.translate("MainWindow", u"Mode", None))
+        self.Account_button.setText(QCoreApplication.translate("MainWindow", u"Account", None))
+        self.LeftSide_menu.setProperty(u"Animation", "")
+        self.Charts_button.setText(QCoreApplication.translate("MainWindow", u"Charts", None))
+        self.Exchanges_button.setText(QCoreApplication.translate("MainWindow", u"Exchanges", None))
+        self.Ai_predictions_button.setText(QCoreApplication.translate("MainWindow", u"Ai predictions", None))
+        self.Arbitrage_button.setText(QCoreApplication.translate("MainWindow", u"Arbitrage pairs", None))
     # retranslateUi
 
--- Main/main.py
+++ Main/main.py
@@ -2,12 +2,19 @@ from PySide6.QtWidgets import QApplication, QWidget
 import sys
 from MainWindow import *
 
-
 class MainWindow(QMainWindow):
     def __init__(self):
         QMainWindow.__init__(self)
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+        self.ui.Menu_button.clicked.connect(self.toggle_left_menu)
+
+    def toggle_left_menu(self):
+        if self.ui.LeftSide_menu.isVisible():
+            self.ui.LeftSide_menu.setVisible(False)
+        else:
+            self.ui.LeftSide_menu.setVisible(True)
+
 
 
 if __name__ == '__main__':
